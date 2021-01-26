221afa2fe248070b4c26ce9473a85ab18f2dad86
Octavio Echeverria <kouze88@gmail.com>
2021-01-25 23:26:43 -0300

    firtscommit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,53 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+ 
+   <nav class="navbar navbar-expand-lg navbar-light bg-custom">
+    <div class="container-fluid text-center">
+      <a class="navbar-brand title" href="#">LightWeb</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon navbar-text"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+      </div>
     </div>
+  </nav>
     <router-view/>
-  </div>
+     <footer class="footer mt-auto py-3">
+    <div class="container">
+      <span class="text-muted">{{copyright+currentDate.getFullYear()+marca}}</span>
+    </div>
+  </footer>
+  </div>   
 </template>
-
+<script>
+export default {
+    data(){
+     return {
+        copyright: '\u00A9',
+        marca: ' LightWeb',    
+        currentDate: new Date,
+        }
+ }
+}
+</script>
 <style>
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -29,4 +68,31 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+@import url('https://fonts.googleapis.com/css2?family=Oleo+Script&family=Roboto:wght@100&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Oleo+Script&family=Press+Start+2P&family=Roboto:wght@100&display=swap');
+.title{
+    font-family: 'Oleo Script', cursive;
+}
+.navbar {
+  background-color: #232f3ee5;
+    border-bottom-left-radius: 15px;
+    border-bottom-right-radius: 15px;
+    border-style: solid;
+    border-width: 2px;
+    border-color: goldenrod;
+}
+.footer {
+    flex-shrink: 0;
+    background-color: #232f3ee5;
+    border-top-left-radius: 15px;
+    border-top-right-radius: 15px;
+    z-index: 1;
+    border-style: solid;
+    border-width: 2px;
+    border-color: goldenrod;
+}
+a {
+    color: honeydew !important
+}
+
 </style>
---
+++ src/components/Particles.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :id="id"></div>
+</template>
+
+<script lang="ts">
+import { Component, Prop } from "vue-property-decorator";
+import { tsParticles } from "tsparticles";
+import type { Container, ISourceOptions } from "tsparticles";
+import Vue from "vue";
+export type IParticlesProps = ISourceOptions;
+export type IParticlesParams = IParticlesProps;
+@Component
+export default class Particles extends Vue {
+  @Prop({ required: true }) private id!: string;
+  @Prop() private options?: IParticlesProps;
+  private particlesContainer?: Container;
+  private mounted(): void {
+    this.$nextTick(() => {
+      if (!this.id) {
+        throw new Error("Prop 'id' is required!")
+      }
+      tsParticles.load(this.id, this.options ?? {}).then(container => this.particlesContainer = container);
+    });
+  }
+  private beforeDestroy(): void {
+    this.particlesContainer?.destroy();
+  }
+}
+</script>
