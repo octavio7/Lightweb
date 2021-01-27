6fa7e48a64734cd81769627589b0458020dca468
Octavio Echeverria <kouze88@gmail.com>
2021-01-26 23:47:50 -0300

    fixHome

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,161 @@
 <template>
-  <div id="app">
- 
-   <nav class="navbar navbar-expand-lg navbar-light bg-custom">
-    <div class="container-fluid text-center">
-      <a class="navbar-brand title" href="#">LightWeb</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon navbar-text"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-          </li>
-        </ul>
+  <div id="app" class="d-flex flex-column">
+    <Particles
+      class="particlescss"
+      id="tsparticles"
+      :options="{
+        background: {
+          color: {
+            value: 'white',
+          },
+        },
+        fpsLimit: 60,
+        interactivity: {
+          detectsOn: 'canvas',
+          events: {
+            onClick: {
+              enable: true,
+              mode: 'push',
+            },
+            onHover: {
+              enable: false,
+              mode: 'repulse',
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 2,
+              opacity: 0.8,
+              size: 40,
+            },
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: '#e6e600',
+          },
+          links: {
+            color: '#e6e600',
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 2,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: 'right',
+            enable: true,
+            outMode: 'out',
+            random: false,
+            speed: 3,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              value_area: 800,
+            },
+            value: 30,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: 'circle',
+          },
+          size: {
+            random: true,
+            value: 6,
+          },
+        },
+        detectRetina: true,
+      }"
+    />
+    <nav class="navbar navbar-expand-lg navbar-light bg-custom">
+      <div class="container-fluid text-center">
+        <a class="navbar-brand title" href="#">LightWeb</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon navbar-text"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link disabled"
+                href="#"
+                tabindex="-1"
+                aria-disabled="true"
+                >Disabled</a
+              >
+            </li>
+          </ul>
+        </div>
       </div>
+    </nav>
+    <div class="d-flex align-items-center altura">
+    <router-view />
     </div>
-  </nav>
-    <router-view/>
-     <footer class="footer mt-auto py-3">
-    <div class="container">
-      <span class="text-muted">{{copyright+currentDate.getFullYear()+marca}}</span>
-    </div>
-  </footer>
-  </div>   
+    <footer class="footer mt-auto py-3 align-self-end">
+      <div class="container">
+        <span class="text-muted">{{
+          copyright + currentDate.getFullYear() + marca
+        }}</span>
+      </div>
+    </footer>
+  </div>
 </template>
 <script>
+import Particles from "@/components/Particles.vue"; // @ is an alias to /src
+  
 export default {
-    data(){
-     return {
-        copyright: '\u00A9',
-        marca: ' LightWeb',    
-        currentDate: new Date,
-        }
- }
-}
+  components: {
+    Particles,
+  },
+  data() {
+    return {
+      copyright: "\u00A9",
+      marca: " LightWeb",
+      currentDate: new Date(),
+    };
+  },
+};
 </script>
 <style>
-
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align:left;
   color: #2c3e50;
+  height: 100vh;
 }
 
 #nav {
@@ -68,31 +170,39 @@ export default {
 #nav a.router-link-exact-active {
   color: #42b983;
 }
-@import url('https://fonts.googleapis.com/css2?family=Oleo+Script&family=Roboto:wght@100&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Oleo+Script&family=Press+Start+2P&family=Roboto:wght@100&display=swap');
-.title{
-    font-family: 'Oleo Script', cursive;
+@import url("https://fonts.googleapis.com/css2?family=Oleo+Script&family=Roboto:wght@100&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Oleo+Script&family=Press+Start+2P&family=Roboto:wght@100&display=swap");
+.title {
+  font-family: "Oleo Script", cursive;
 }
 .navbar {
   background-color: #232f3ee5;
-    border-bottom-left-radius: 15px;
-    border-bottom-right-radius: 15px;
-    border-style: solid;
-    border-width: 2px;
-    border-color: goldenrod;
+  border-bottom-left-radius: 15px;
+  border-bottom-right-radius: 15px;
+  border-style: solid;
+  border-width: 2px;
+  border-color: goldenrod;
 }
 .footer {
-    flex-shrink: 0;
-    background-color: #232f3ee5;
-    border-top-left-radius: 15px;
-    border-top-right-radius: 15px;
-    z-index: 1;
-    border-style: solid;
-    border-width: 2px;
-    border-color: goldenrod;
+  background-color: #232f3ee5;
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+  z-index: 1;
+  border-style: solid;
+  border-width: 2px;
+  border-color: goldenrod;
+  width: 100%;
 }
 a {
-    color: honeydew !important
+  color: honeydew !important;
 }
-
+ .particlescss{
+  position: absolute;
+  width: 100%;
+  height: 90%;
+  z-index: -1;
+}
+.altura{
+  min-height:79%;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,91 +1,10 @@
 <template>
-  <div class="home">
-         <Particles
-     class="particlescss"
-      id="tsparticles"
-      :options="{
-            background: {
-                color: {
-                    value: 'white'
-                }
-            },
-            fpsLimit: 60,
-            interactivity: {
-                detectsOn: 'canvas',
-                events: {
-                    onClick: {
-                        enable: true,
-                        mode: 'push'
-                    },
-                    onHover: {
-                        enable: false,
-                        mode: 'repulse'
-                    },
-                    resize: true
-                },
-                modes: {
-                    bubble: {
-                        distance: 400,
-                        duration: 2,
-                        opacity: 0.8,
-                        size: 40
-                    },
-                    push: {
-                        quantity: 4
-                    },
-                    repulse: {
-                        distance: 200,
-                        duration: 0.4
-                    }
-                }
-            },
-            particles: {
-                color: {
-                    value: '#e6e600'
-                },
-                links: {
-                    color: '#e6e600',
-                    distance: 150,
-                    enable: true,
-                    opacity: 0.5,
-                    width: 1
-                },
-                collisions: {
-                    enable: true
-                },
-                move: {
-                    direction: 'right',
-                    enable: true,
-                    outMode: 'out',
-                    random: false,
-                    speed: 4,
-                    straight: false
-                },
-                number: {
-                    density: {
-                        enable: true,
-                        value_area: 800
-                    },
-                    value: 30
-                },
-                opacity: {
-                    value: 0.5
-                },
-                shape: {
-                    type: 'circle'
-                },
-                size: {
-                    random: true,
-                    value: 5
-                }
-            },
-            detectRetina: true
-        }"
-    />
-     <main class="container content d-flex">
-    <div class="align-self-center">
-      <h1 class="inner_txt">Páginas y aplicaciones web increíbles a tu alcance!</h1>
-      <p>Descubre cómo una pagina web profesional, impulsa tu negocio Online. No basta con tener presencia en internet
+  <div class="home container">
+
+     <main>
+    <div>
+      <h1>Páginas y aplicaciones web increíbles a tu alcance!</h1>
+      <p class="text-start fw-bold">Descubre cómo una página web profesional, impulsa tu negocio Online. No basta con tener presencia en internet
         para competir, necesitas estar a la última en tecnologías web para cumplir tus metas.
         Todo lo que necesitas...</p>
          <Boton />
@@ -99,13 +18,11 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import Particles from '@/components/Particles.vue'; // @ is an alias to /src
 import Boton from '@/components/Boton.vue'; // @ is an alias to /src
 
 
 @Component({
   components: {
-    Particles,
     Boton
   },
 })
@@ -122,17 +39,5 @@ export default class Home extends Vue {}
     font-family: 'Press Start 2P', cursive;
     margin-bottom:1em;
 }
- main{
-   min-height:80vh;
-}
- .particlescss{
-  position: absolute;
-  width: 100%;
-  height: 90vh;
-  z-index: -1;
-}
-.btn{
-  color:red;
-  z-index: 2;
-}
+
 </style>
